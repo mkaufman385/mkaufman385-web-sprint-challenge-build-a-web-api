f303b1f87a593b224f896c08e28dadc92b4510fb
mkaufman385 <mkaufman385@gmail.com>
2024-03-07 11:52:53 -0500

    wip

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ const server = express();
 
 server.use(express.json());
 
-server.get("api/projects", (req, res, next) => {
+server.get("api/projects", (req, res) => {
   res.json({ message: "API is working " });
+});
+
+server.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
 });
 
 console.log(process.env.PORT, process.env.NODE_ENV);

03c7b9a4330d2800cc05143aada2b7da16a5db9a
mkaufman385 <mkaufman385@gmail.com>
2024-03-07 12:33:35 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -1,5 +1,15 @@
-const express = require('express');
+const express = require("express");
 const server = express();
+
+server.use(express.json());
+
+// server.get("/", (req, res) => {
+//   res.json({ message: "API is working " });
+// });
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Lets do it !!!</h2>`);
+});
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 
-const server = express();
+const server = require("./api/server");
 
 const PORT = process.env.PORT || 9000;
 
-server.use(express.json());
+// server.use(express.json());
 
 server.get("api/projects", (req, res) => {
   res.json({ message: "API is working " });
 });
 
-server.use("*", (req, res) => {
-  res.send(`<h1>Project working!!</h1>`);
-});
+// server.use("*", (req, res) => {
+//   res.send(`<h1>Project working!!</h1>`);
+// });
 
 server.use((err, req, res, next) => {
   res.status(500).json({

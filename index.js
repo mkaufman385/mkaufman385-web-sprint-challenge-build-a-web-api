a3b4c3f6def51de5e2ef36cbef93f861c51a4e25
mkaufman385 <mkaufman385@gmail.com>
2024-03-07 11:50:32 -0500

    wip

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 require("dotenv").config();
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("api/projects", (req, res, next) => {
+  res.json({ message: "API is working " });
+});
 
 console.log(process.env.PORT, process.env.NODE_ENV);

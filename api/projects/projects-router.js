975961056b7214f5eda7bed587358b5db9075e62
mkaufman385 <mkaufman385@gmail.com>
2024-03-07 13:07:52 -0500

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,16 @@
-// Write your "projects" router here!
+const express = require("express");
+const router = express.Router();
+
+module.exports = router;
+
+router.get("/api/projects", (req, res) > {});
+
+router.get("/api/projects/:id", (req, res) > {});
+
+router.post("/api/projects", (req, res) > {});
+
+router.put("/api/projects/:id", (req, res) > {});
+
+router.delete("/api/projects/:id", (req, res) > {});
+
+router.get("/api/projects", (req, res) > {});

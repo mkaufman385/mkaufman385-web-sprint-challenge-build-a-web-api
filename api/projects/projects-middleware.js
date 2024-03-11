56a789c814ba6cb858690a84595b6800b6796a7a
mkaufman385 <mkaufman385@gmail.com>
2024-03-11 15:32:28 -0400

    wip

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,23 +1,15 @@
 const Project = require("./projects-model");
 
-async function validateProject(req, res, next) {
-  try {
-    const { name, description } = req.body;
+function validateProject(req, res, next) {
+  const { name, description } = req.body;
 
-    if (!name || !description) {
-      return res.status(400).json({
-        message:
-          "Please provide both 'name' and 'description' in the request body.",
-      });
-    }
-    next();
-  } catch (err) {
-    console.error("Error:", err);
-    res.status(500).json({
-      message: "Internal Server Error during validation",
-      err: err.message,
-      stack: err.stack,
+  if (!name || !description) {
+    return res.status(400).json({
+      message:
+        "Please provide both 'name' and 'description' in the request body.",
     });
+  } else {
+    next();
   }
 }
 

fd08665a420faa6532e52e0baf5efd4bc0fca75b
mkaufman385 <mkaufman385@gmail.com>
2024-03-11 20:26:36 -0400

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,16 @@
-// Write your "actions" router here!
+const express = require("express");
+const Action = require("./actions-model");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {});
+
+router.get("/:id", (req, res) => {});
+
+router.post("/", (req, res) => {});
+
+router.put("/:id", (req, res) => {});
+
+router.delete("/:id", (req, res) => {});
+
+module.exports = router;

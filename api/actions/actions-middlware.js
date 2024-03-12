f7a3e74a5789e7304920f4dc46aa66e19b5fc533
mkaufman385 <mkaufman385@gmail.com>
2024-03-12 15:45:31 -0400

    wip

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,15 @@
 // add middlewares here related to actions
+function validateAction(req, res, next) {
+  console.log("validateAction");
+  next();
+}
+
+function validateActionId(req, res, next) {
+  console.log("validateActionId");
+  next();
+}
+
+module.exports = {
+  validateAction,
+  validateActionId,
+};
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -5,12 +5,19 @@ const router = express.Router();
 
 router.get("/", (req, res) => {});
 
-router.get("/:id", (req, res) => {});
+// router.get("/:id", (req, res) => {});
 
-router.post("/", (req, res) => {});
+// router.post("/", (req, res) => {});
 
-router.put("/:id", (req, res) => {});
+// router.put("/:id", (req, res) => {});
 
-router.delete("/:id", (req, res) => {});
+// router.delete("/:id", (req, res) => {});
 
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    customMessage: "Soemthing tragic inside posts router happened",
+    err: err.message,
+    stack: err.stack,
+  });
+});
 module.exports = router;
